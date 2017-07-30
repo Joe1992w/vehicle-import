a5ad16f94954270dd43a2e6be9119dbcd846b6ef
Joe Ware <joe_ware@hotmail.co.uk>
2017-07-30 13:07:30 +0100

    Added Filters to Vehicles
    Added Filtering to Vehicle Controller and View using the new scopes

--- app/Http/Controllers/VehicleController.php
+++ app/Http/Controllers/VehicleController.php
@@ -3,6 +3,13 @@
 namespace App\Http\Controllers;
 
 use App\Models\Vehicle;
+use App\Models\VehicleManufacturer;
+use App\Models\VehicleColour;
+use App\Models\VehicleTransmission;
+use App\Models\VehicleType;
+use App\Models\FuelType;
+use App\Models\Owner;
+use App\Models\VehicleModel;
 use Illuminate\Http\Request;
 
 class VehicleController extends Controller
@@ -12,11 +19,67 @@ class VehicleController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
         //
-        $vehicles = Vehicle::with('model', 'model.manufacturer', 'owner')->paginate(10);
-        return view('vehicles.index')->with(compact('vehicles'));
+        $vehicles = Vehicle::with('model', 'model.manufacturer', 'owner');
+
+        if($request->input('manufacturer')) {
+            $manufacturer = VehicleManufacturer::findOrFail($request->input('manufacturer'));
+            $vehicles = $vehicles->whereManufacturer($manufacturer);
+        }
+
+        if($request->input('model')) {
+            $model = VehicleModel::findOrFail($request->input('model'));
+            $vehicles = $vehicles->whereModel($model);
+        }
+
+        if($request->input('colour')) {
+            $colour = VehicleColour::findOrFail($request->input('colour'));
+            $vehicles = $vehicles->whereColour($colour);
+        }
+
+        if($request->input('transmission')) {
+            $transmission = VehicleTransmission::findOrFail($request->input('transmission'));
+            $vehicles = $vehicles->whereTransmission($transmission);
+        }
+
+        if($request->input('type')) {
+            $type = VehicleType::findOrFail($request->input('type'));
+            $vehicles = $vehicles->whereType($type);
+        }
+
+        if($request->input('fuel')) {
+            $fuel = FuelType::findOrFail($request->input('fuel'));
+            $vehicles = $vehicles->whereFuelType($fuel);
+        }
+
+        if($request->input('owner')) {
+            $owner = Owner::findOrFail($request->input('owner'));
+            $vehicles = $vehicles->whereOwner($owner);
+        }
+
+        $vehicles = $vehicles->paginate(10)
+            ->appends($request->only(['manufacturer', 'model', 'colour', 'transmission', 'type', 'fuel', 'owner']));
+
+        $manufacturers = VehicleManufacturer::orderBy('name')->get();
+        $models= VehicleModel::orderBy('name')->get();
+        $colours = VehicleColour::orderBy('name')->get();
+        $transmissions = VehicleTransmission::orderBy('name')->get();
+        $types = VehicleType::orderBy('name')->get();
+        $fuels = FuelType::orderBy('name')->get();
+        $owners = Owner::orderBy('name')->get();
+
+        return view('vehicles.index')->with(compact([
+            'vehicles',
+            'manufacturers',
+            'models',
+            'colours',
+            'transmissions',
+            'types',
+            'fuels',
+            'owners'
+        ]));
     }
 
     /**
--- resources/views/vehicles/index.blade.php
+++ resources/views/vehicles/index.blade.php
@@ -6,6 +6,39 @@
 		<h1>Vehicles</h1>
 
 		<div class="panel panel-default">
+			<div class="panel-heading"><h4>Filter Vehicles</h4></div>
+			<div class="panel-body">
+				<form class="form">
+
+					<div class="row">
+
+						@foreach(['manufacturer', 'model', 'colour', 'transmission', 'type', 'fuel', 'owner'] as $filter)
+							<div class="col-sm-6 col-md-4 col-lg-3">
+								<div class="form-group">
+									<label for="{{ $filter }}-filter">{{ ucfirst($filter) }}</label>
+									<select name="{{ $filter }}" id="{{ $filter }}-filter" class="form-control">
+										<option value="">All {{ ucfirst($filter) }}s</option>
+										@foreach(${$filter.'s'} as ${$filter})
+											<option value="{{ ${$filter}->id }}" @if(request($filter) == ${$filter}->id) selected @endif>{{ ${$filter}->name }}</option>
+										@endforeach
+									</select>
+								</div>
+							</div>
+						@endforeach
+
+					</div>
+
+					<div class="row">
+						<div class="col-xs-12">
+							<button class="btn-primary btn" type="submit">Filter Vehicles</button>
+						</div>
+					</div>
+
+				</form>
+			</div>
+		</div>		
+
+		<div class="panel panel-default">
 			@include('_partials/vehicles/vehicles-table', compact('vehicles'))
 		</div>
 
